c4c1a95b7a2f9fd444a97c8c3514d19ec13d71b1
ameya <44024565+ameyalizbeth@users.noreply.github.com>
2021-06-26 15:42:11 +0530

    signup corrected

---
+++ backEnd/src/models/mails.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const validator = require('validator');
+const Schema = mongoose.Schema;
+const mail = new Schema({
+    toEmail: {
+        type: String,
+        required: true,
+        
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid')
+            }
+        }
+    },
+    fromEmail: {
+        type: String,
+        required: true,
+        
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid')
+            }
+        }
+    },
+    subject: {
+        type: String,
+       
+        
+    },
+    body: {
+        type: string,
+        
+    },
+    sent: {
+        type: Boolean,
+        required:true
+    }
+      
+        
+    
+
+    
+});
+
+module.exports = mongoose.model('mail', mail);

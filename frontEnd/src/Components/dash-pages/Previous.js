6c1819fdf0b49498ca817e7cd7cec1fa3d21fa12
Hirafathima <hirafathima125@gmail.com>
2021-06-27 21:25:45 +0530

    home page

--- frontEnd/src/Components/dash-pages/Previous.js
+++ frontEnd/src/Components/dash-pages/Previous.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import Axios from "axios";
 import './prev.css'
 import {initData} from './data'
 
 function Previous(){
+
+    const user = localStorage.getItem("email");
+    const [data, setData] = useState("");
+
+
+    useEffect(() => {
+        Axios.get(`http://localhost:8001/user/api/sendmails/${user}`, {
+            headers: {
+                "x-access-token": localStorage.getItem("token"),
+            },
+        }).then((response) => {
+            console.log(response);
+            setData(response.data.result);
+        });
+    }, []);
+
     return(
 
         <div>
             <div style={{marginBottom:20}}>Previous Emails</div>
             <div class="container">
                 <div className="row prev-row">
-            {initData.map((item)=>{
+            {data==="" || data===undefined?"":data.map((item)=>{
                 return(
-                    
-                    <div className="col-xl-6 card prev-card">
-                        <div className="card-pad">{item.name}</div>
-                    </div>     
+                   <div>{item===""?"":<div className="col-xl-6 card prev-card">
+                   <div className="card-pad">{item.subject}
+                   <p>{item.toEmail.length}</p>
+                   </div>
+
+               </div>}</div>
+                         
                 )
             })}
             </div>
--- frontEnd/src/Components/dash-pages/Schedule.js
+++ frontEnd/src/Components/dash-pages/Schedule.js
@@ -1,30 +1,33 @@
-import React,{useEffect} from 'react';
+import React,{useState, useEffect} from 'react';
 import './schedule.css';
-import {initData} from './data';
 import Axios from "axios";
 
 function Schedule(){
     const user = localStorage.getItem("email");
+    const [data, setData] = useState("");
+
 
     useEffect(() => {
-        Axios.get(`http://localhost:8001/user/api/sendmails/${user}`, {
+        Axios.get(`http://localhost:8001/user/api/${user}/homepage`, {
             headers: {
                 "x-access-token": localStorage.getItem("token"),
             },
         }).then((response) => {
             console.log(response);
+            setData(response.data.emails);
         });
     }, []);
     return (
         <div>
             <div style={{marginBottom:20}}>Scheduled Emails</div>
 
-            {initData.map((item)=>{
+            {data==="" || data===undefined?"":data.map((item)=>{
                 return(
                     <div className="sche-card">
                         <div>
-                            <div className="mail-desc">{item.qst}</div>
-                            <div className="sche-time">Time | Date</div>
+                            <div className="mail-desc">{item.subject}</div>
+                <div className="sche-time">{item.dateAndTime===undefined?"":(item.dateAndTime.split(" "))[2]} {item.dateAndTime===undefined?"":(item.dateAndTime.split(" "))[1]} {item.dateAndTime===undefined?"":(item.dateAndTime.split(" "))[0]} | {item.dateAndTime===undefined?"":(item.dateAndTime.split(" "))[4]} | {item.category} | Audience:{item.toEmail.length}</div>
+                        
                         </div>
                     </div>
                 )
